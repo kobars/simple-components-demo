0e2f3201f8b758ab9e7722bf69258e3bbc28582c
Kobar <kobar@alterra.id>
2020-04-30 12:26:09 +0700

    add utils

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export const mentees = [
+    { name: 'Aji', content: 'Lagi sendiri aja nih', hashTag: 'Menunggumu' },
+    { name: 'Derby', content: 'Lagi berdua aja nih', hashTag: 'AwasSetan' },
+    { name: 'Bagas', content: 'Lagi bertiga aja nih', hashTag: 'BukanSetan' }
+]
